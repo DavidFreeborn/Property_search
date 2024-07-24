28d3ad8a46b62302c5dcc52caf141a9d9348c2a9
DavidFreeborn <153205930+DavidFreeborn@users.noreply.github.com>
2024-07-24 20:16:27 +0100

    Add files via upload

--- local-server.py
+++ local-server.py
@@ -11,5 +11,6 @@ Handler.extensions_map.update({
 print(f"Serving at http://localhost:{PORT}")
 print("To view the map, open a web browser and go to:")
 print(f"http://localhost:{PORT}/property_map.html")
+print(f"http://localhost:{PORT}/estate_agents_map.html")
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     httpd.serve_forever()
